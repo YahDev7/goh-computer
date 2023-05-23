eb9c1ade4adbb8263f78e38ee5d63bcbc5db5dfb
wiel <yahiesdev@gmail.com>
2023-05-22 19:19:45 -0500

    barra

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     }); */
    app.enableCors({
     credentials: true,
-    origin:['http://127.0.0.1:5173','https://boisterous-kringle-fbc71c.netlify.app','https://gentle-beijinho-73a6a3.netlify.app/'],
+    origin:['http://127.0.0.1:5173','https://boisterous-kringle-fbc71c.netlify.app','https://gentle-beijinho-73a6a3.netlify.app'],
     methods: ["GET", "POST"],
   }) 
   app.useGlobalPipes(new ValidationPipe({
