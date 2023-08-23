f1780882e6f0b8b0aa8401b3507eab416abd6b05
wiel <yahiesdev@gmail.com>
2023-08-23 12:26:07 -0500

    url

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     }); */
    app.enableCors({
     credentials: true,
-    origin:['http://127.0.0.1:5173','https://boisterous-kringle-fbc71c.netlify.app'],
+    origin:['http://127.0.0.1:5173','https://boisterous-kringle-fbc71c.netlify.app','https://gohcomputer.netlify.app'],
     methods: ["GET", "POST","PUT","DELETE"],
   }) 
   app.useGlobalPipes(new ValidationPipe({
